93c2d75ca6f994610dd6e9d079bb2c131903523c
Md. Abdullah <82255938+MdAbdullah2010@users.noreply.github.com>
2021-04-19 11:55:00 +0600

    series_sum
    doing series.

---
+++ Loop/Series1.cpp
@@ -0,0 +1,18 @@
+///1+2+3+.....nth term
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float n,z=0;
+    cin>>n;
+    /*
+    for(int i=1;i<=n;i++)
+    {
+        z=z+i;
+    }
+    */
+    z = n/2*(n+1);
+    cout<<z;
+    return 0;
+}
